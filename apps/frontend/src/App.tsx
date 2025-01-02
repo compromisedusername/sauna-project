6486c93b1a5ad9a49170102bb54fc9934d87b879
barteko <haldman123@gmail.com>
2025-01-02 17:48:11 +0100

    feat(routing): add basic routes

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,42 +1,48 @@
 import React from "react";
-import { BrowserRouter, Routes, Route,  } from "react-router-dom";
-import {useState} from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
 import Login from "./components/Login/Login";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Register from "./components/Register/Register";
 import Preferences from "./components/Preferences/Preference";
 
-function setToken(userToken: string){
-localStorage.setItem("token", userToken);
+function setToken(userToken: string) {
+  localStorage.setItem("token", userToken);
 }
-function getToken(): string{
-return localStorage.getItem("token") || "guest";
+function getToken(): string | null {
+  return localStorage.getItem("token");
 }
-
 
 function App() {
-
-const token = getToken();
-
-  if(!token){
-    return <Login setToken={setToken}/>
-  }
-
+  const token = getToken();
 
 
   return (
     <div className="wrapper">
       <h1>Sauna reservation</h1>
       <BrowserRouter>
-      <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/preferences" element={<Preferences />} />
-          <Route path="/login" element = {<Login setToken={setToken}/>}/>
-          <Route path="/register" element = {<Register setToken={setToken}/>}/>
+        <Routes>
+          <Route path='/' element = {<Login setToken={setToken}></Login>}></Route>
+          {token ? (
+            <>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/preferences" element={<Preferences />} />
+              <Route path ="/logout" element={<></>}/>
+              <Route path="/dashboard/admin"/>
+
+            </>
+          ) : (
+            <>
+              <Route path="/login" element={<Login setToken={setToken} />} />
+              <Route
+                path="/register"
+                element={<Register setToken={setToken} />}
+              />
+            </>
+          )}
         </Routes>
       </BrowserRouter>
-
     </div>
   );
 }
